956aaa4284b24dcb66be7854e717de23144754e3
Matevz Vucnik <vucnikm@gmail.com>
2016-12-19 11:53:13 +0100

    add try except and sleep on fail

--- sunn-proxy.py
+++ sunn-proxy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import time
 import pika
 import paho.mqtt.client as mqtt
 
@@ -8,24 +9,30 @@ EXCHANGE = "wams_exchange"
 QUEUE_NAME = "wams_save_queue"
 ROUTING_KEY = "wams.spm"
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST))
-channel = connection.channel()
-channel.exchange_declare(exchange=EXCHANGE,
-                         type="topic", durable=True)
+try:
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST))
+    channel = connection.channel()
+    channel.exchange_declare(exchange=EXCHANGE,
+                             type="topic", durable=True)
 
-def on_connect(client, userdata, rc):
-    print("Connected with result code "+str(rc))
-    client.subscribe("spm/+")
+    def on_connect(client, userdata, rc):
+        print("Connected with result code "+str(rc))
+        client.subscribe("spm/+")
 
-def on_message(client, userdata, msg):
-    if '"report_n":49' in msg.payload:
-        channel.basic_publish(exchange=EXCHANGE,
-                          routing_key=ROUTING_KEY,
-                          body=str(msg.payload))
+    def on_message(client, userdata, msg):
+        if '"report_n":49' in msg.payload:
+            channel.basic_publish(exchange=EXCHANGE,
+                              routing_key=ROUTING_KEY,
+                              body=str(msg.payload))
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect("localhost", 1883)
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect("localhost", 1883)
 
-client.loop_forever()
+    client.loop_forever()
+
+except:
+    print "Fail!"
+    time.sleep(10)
+    exit(1)
